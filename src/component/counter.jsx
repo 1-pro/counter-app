feea370219af630910e82fa6375b8cb7d57daf60
andrew okiki <andrewokiki01@gmail.com>
2021-12-27 03:58:26 +0100

    first practice commit

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 class Counter extends React.Component {
-    render() { 
+    state ={
+        count: 0,
+        tags: ['tag1','tag2','tag3']
+    };
+
+    /*renderTags(){
+        if(this.state.tags.length===0) return <p>"There are no tags"</p>;
+        return   <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    };
+
+    render(){
+        return <React.Fragment>
+            {this.state.tags.length===0 && 'Please add an item'}
+            {this.renderTags()}
+        </React.Fragment>
+    }*/
+    /*constructor(){
+        super()
+       this.handleIncrement= this.handleIncrement.bind(this);
+    }*/
+
+    handleIncrement=()=>{
+        this.setState({count:this.state.count + 1});
+      //                                                                                                                                                                                                                                                                                                                                                                   console.log('Increment Click', this)
+    }
+
+   render() { 
         return( 
             <React.Fragment>
-                <h1>Hello world</h1>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onCLick={this.handleIncrement} className='btn btn-sm btn-secondary'>Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
             );
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount(){
+       // return this.state.count === 0 ? 'Zero' : this.state.count
+       const{count}= this.state;
+       return count === 0 ? 'Zero' : count;
     }
 }
  
