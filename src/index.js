e81695c26d3569aac108dc6ade2523916df1dada
Andrew Okiki <andrewokiki01@gmail.com>
2022-07-08 05:09:57 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './component/Counter';
 import 'bootstrap/dist/css/bootstrap.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'font-awesome/css/font-awesome.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<App />,document.getElementById('root'));
 reportWebVitals();
